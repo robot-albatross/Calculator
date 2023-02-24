f9013c8d647a4ad0c20e2f5d83f91b39f32abb2a
Pablo <91420916+robot-albatross@users.noreply.github.com>
2023-02-24 13:56:09 -0300

    feat. Add calculator buttons and functionalities

--- index.js
+++ index.js
@@ -1,27 +1,107 @@
 //Calculator Operations
 
 function add(a,b) {
+    if ((a === undefined && b === undefined)) {
+        return 0;
+    }
+    if (b === undefined) {
+        return a;
+    }
     return a + b;
 }
 
 function subtract(a,b) {
-    return a - b;
+    if ((a === undefined && b === undefined)) {
+        return 0;
+    }
+    if (b === undefined) {
+        return a;
+    }
+
+    if (a < 0) {
+        return -1*(Math.abs(a) - Math.abs(b));
+    } else {
+        return Math.abs(a) - Math.abs(b);
+    }
 }
 
 function multiply(a,b) {
+    if ((a === undefined && b === undefined)) {
+        return 0;
+    }
+    if (b === undefined) {
+        return a;
+    }
     return a * b;
 }
 
 function division(a,b) {
-    return Math.round((a / b) * 100) / 100;
-}
-
-function operate(operation, a, b) {
-    operation(a,b);
+    if ((a === undefined && b === undefined)) {
+        return 0;
+    }
+    if (b === undefined) {
+        return a;
+    }
+    if (b/a < 1) {
+        return Math.round(b/a * 100) / 100;
+    } else {
+        return b/a;
+    }
 }
 
 //keys events
 
-buttons = document.querySelector(".buttons");
-console.log(buttons);
+let lastKey = {lastKeyPressedWasSign: false, lastKeySignPressed:""};
+
+displayValues = {
+    pastDisplayValue: undefined,
+    currentDisplayValue: undefined,
+};
+
+display = document.querySelector(".display");
+values = document.querySelectorAll(".buttons > .value");
+values.forEach(value => {
+    value.addEventListener("click", function eventHandler(e) {
+        
+if (lastKey.lastKeyPressedWasSign) {
+            display.innerHTML = "";
+        }
+        e.stopImmediatePropagation();
+        display.innerHTML += value.innerHTML;
+        lastKey.lastKeyPressedWasSign = false;
+    });
+});
+
+mathSigns = document.querySelectorAll(".buttons > .mathSign");
+mathSigns.forEach(mathSign => {
+    mathSign.addEventListener("click", function eventHandler(e) {
+        function configureMathSignButton(operation) {
+            displayValues.currentDisplayValue = Number(display.innerHTML);
+            display.innerHTML = operation(displayValues.currentDisplayValue, displayValues.pastDisplayValue).toString();
+        }
+        function selectOperation(symbol) {
+            console.log(symbol);
+            if (symbol === "+") {
+                configureMathSignButton(add);
+            }    
+            if (symbol === "-") {
+                configureMathSignButton(subtract);
+            }
+            if (symbol === "*") {
+                configureMathSignButton(multiply);
+            }
+            if (symbol === "/") {
+                configureMathSignButton(division);
+            }
+        }
+
+        if ((!(lastKey.lastKeyPressedWasSign)) && displayValues.pastDisplayValue === undefined); {
+            selectOperation(lastKey.lastKeySignPressed);
+        }
+        e.stopImmediatePropagation();
+        displayValues.pastDisplayValue = Number(display.innerHTML);
+        lastKey.lastKeySignPressed = mathSign.innerHTML;
+        lastKey.lastKeyPressedWasSign = true;
+    })
+});
 
